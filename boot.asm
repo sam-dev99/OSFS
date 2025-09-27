b7888ff221a258d40a347b41c48323df99c04f50
sam-dev99 <sabbahsamir1999@gmail.com>
2025-09-27 20:43:52 +0200

    optimized bootloader

--- boot.asm
+++ boot.asm
@@ -1,7 +1,20 @@
-ORG 0X7C00
+ORG 0
 BITS 16
 
+jmp 0x7c0:start
+
 start:
+    cli ;clear interrupts
+    sti ;enables interrupts
+
+    mov ax, 0x7c0
+    mov ds,ax
+    mov es, ax
+
+    mov ax, 0x00
+    mov ss,ax
+    mov sp,0x7c00
+
     mov si, message
     call print
     jmp $
